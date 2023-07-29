1acc9b49e5486a0a125e70f6e9b4fa47afc850bc
Joao Luiz <joaoluizmarcelinoribeiro@gmail.com>
2023-07-28 23:29:25 -0300

    criando navegacao para telas

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -1,29 +1,42 @@
 import 'package:flutter/material.dart';
+
 import '../models/category.dart';
+import '../screens/categories_meals_screens.dart';
 
 class CategoryItem extends StatelessWidget {
   final Category category;
 
-  const CategoryItem(this.category, {Key? key}) : super(key: key);
+  const CategoryItem(this.category, {super.key});
+
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(builder: (_) {
+      return CategoriesMealsScreen(category);
+    }));
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-        gradient: LinearGradient(
-          colors: [
-            category.color.withOpacity(0.5),
-            category.color,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      onTap: () => _selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.5),
+              category.color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
         ),
-      ),
-      child: Text(
-        category.title,
-        style: Theme.of(context).textTheme.titleLarge,
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
       ),
     );
   }
