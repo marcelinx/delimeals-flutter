c003fd9c39c1361f2f2d292b5fa23253eb419118
Joao Luiz <joaoluizmarcelinoribeiro@gmail.com>
2023-07-29 00:19:30 -0300

    corrigindo erro

--- lib/screens/categories_screens.dart
+++ lib/screens/categories_screens.dart
@@ -9,17 +9,17 @@ class CategoriesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Vamos cozinhar?'),
+        title: const Text('Vamos Cozinhar?'),
       ),
       body: GridView(
-        padding: const EdgeInsets.all(15),
+        padding: const EdgeInsets.all(25),
         gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
           maxCrossAxisExtent: 200,
           childAspectRatio: 3 / 2,
           crossAxisSpacing: 20,
           mainAxisSpacing: 20,
         ),
-        children: DUMMY_CATEGORIES.map((cat) {
+        children: dummyCategories.map((cat) {
           return CategoryItem(cat);
         }).toList(),
       ),
